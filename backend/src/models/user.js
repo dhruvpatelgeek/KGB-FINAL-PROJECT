0ddad295f39c4c921451e7cf97acd64d6aa12986
Fengpeng Huang <patrickwong.ubcengineering@gmail.com>
2021-11-28 15:31:53 -0800

    add timeStamp and created by to the database of a user

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose')
 
+const opts = {
+    // Make Mongoose use Unix time (seconds since Jan 1, 1970)
+    timestamps: { currentTime: () => Math.floor(Date.now() / 1000) },
+};
+
 const User = mongoose.model('User',{
     fingerprint:{
         type: String
     },
+    timestamps: {
+        type: Date,
+        default: Date.now
+    },
+    createdBy: {
+        type: String,
+        default: "UBC"
+    }
 })
 
 
